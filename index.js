8109f140c38f86525440292400fdfec7c52ca9bf
Marden Batista <marden@Mardens-MacBook-Air.local>
2020-06-15 19:25:50 -0300

    Update Response
    Updates API response to test CI

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app =  express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/',(request, response) => {
-    response.send({ hi: 'there'});
+    response.send({ API_VERSION: '0.0.1'});
 });
 
 app.listen(PORT);
